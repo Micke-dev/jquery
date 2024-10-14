5735f6eef01d4cace66e82873e1ed225f0e7a652
Micke-dev <mickeiuzo.ms@gmail.com>
2024-10-14 15:28:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
     $('header button').click(function(){
-        
+        $('form').slideDown();
     })
 
     $('form').on('submit',function(e) {
         e.preventDefault();
-        const tarefa = $('tarefa').val();
-        const novoItem = $('<li></li>');
-        $(`<div class="nova-tarefa">
-
-            </div>
-        `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
+        const enderecoDaNovaTarefa = $('#endereco-tarefa-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<li="${enderecoDaNovaTarefa}" />`).appendTo(novoItem);
+        
     })
 })
+
